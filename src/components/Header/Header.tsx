76d72b20dfad1ac5ee11629e89fa632097104387
Manuel Atance <atance3@hotmail.com>
2023-02-06 21:21:03 +0100

    Added header logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 
-import { ThemeContextType, Theme } from "./../../context/theme"
+import { ThemeContextType } from "./../../context/theme"
 import { ThemeContext } from "./../../context/ThemeContext"
 
 import dictionaryLogo from "./../../assets/images/logo.svg"
@@ -8,9 +8,15 @@ import arrowDown from "./../../assets/images/icon-arrow-down.svg"
 import dmIcon from "./../../assets/images/icon-moon.svg"
 
 export function Header() {
+	const [fontModal, setFontModal] = useState(false)
 	const { appTheme, changeTheme } = React.useContext(ThemeContext) as ThemeContextType
+
 	const handleThemeToggle = () => {
 		appTheme === "light" ? changeTheme("dark") : changeTheme("light")
+	}
+
+	const openFontModal = () => {
+		setFontModal(true)
 	}
 
 	return (
@@ -21,9 +27,20 @@ export function Header() {
 			<div className="headerControls">
 				<div className="fontPicker">
 					<p>Sans Serif</p>
-					<button>
+					<button onClick={() => openFontModal()}>
 						<img src={arrowDown} alt="arrow down" />
 					</button>
+					<div className="fontSelector" style={fontModal ? { display: "block" } : { display: "none" }}>
+						<button>
+							<p style={{ fontFamily: "var(--sans-serif)" }}>Sans Serif</p>
+						</button>
+						<button>
+							<p style={{ fontFamily: "var(--serif)" }}>Serif</p>
+						</button>
+						<button>
+							<p style={{ fontFamily: "var(--mono)" }}>Mono</p>
+						</button>
+					</div>
 				</div>
 				<div className="dmControls">
 					<label className="dmSwitch">
